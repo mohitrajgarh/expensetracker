f60a2c4dd11f339b0d91c0f9a48011360d24d476
rahulsaini10 <rkst31324@gmail.com>
2025-01-25 15:19:31 +0530

    design front-end

---
+++ script.js
@@ -0,0 +1,10 @@
+const expenseForm = document.getElementById('expenseForm')
+
+const expenseList = document.getElementById('ExpenseList')
+
+expenseForm.addEventListener('submit',
+    function(event){
+        event,preventDefault();
+
+         const description = document.getElementById(')
+    })
